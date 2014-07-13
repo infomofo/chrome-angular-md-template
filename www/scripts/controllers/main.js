6cd5183565484bd6ca90e72e1f4605bc26ce450e
Will Chiong <will@infomofo.com>
2014-07-13 13:27:29 -0400

    add font-awesome and bootstrap (just for font-awesome compatibility) and added an icon for the sidenav

--- www/scripts/controllers/main.js
+++ www/scripts/controllers/main.js
@@ -2,21 +2,19 @@
 
 angular.module('genericChromeMdApp')
   .controller('MainCtrl', function ($scope, $timeout, $materialSidenav) {
-    var leftNav;
-    $timeout(function() {
-      leftNav = $materialSidenav('left');
-    });
-    $scope.toggleLeft = function() {
-      leftNav.toggle();
+    $scope.toggleMenu = function() {
+      $timeout(function() {
+        $materialSidenav('left').toggle();
+      });
     };
   })
   .controller('LeftCtrl', function($scope, $timeout, $materialSidenav) {
-  var nav;
-  $timeout(function() {
-    nav = $materialSidenav('left');
-  });
-  $scope.close = function() {
-    nav.close();
-  };
-})
+    var nav;
+    $timeout(function() {
+      nav = $materialSidenav('left');
+    });
+    $scope.close = function() {
+      nav.close();
+    };
+  })
   ;
