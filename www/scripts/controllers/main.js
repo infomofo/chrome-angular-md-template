d76d096a8e6d5afeda5abd75036af01f012ccc85
Will Chiong <will@infomofo.com>
2014-09-12 23:48:58 -0400

    update to use latest material design demo components

--- www/scripts/controllers/main.js
+++ www/scripts/controllers/main.js
@@ -28,6 +28,51 @@ angular.module('genericChromeMdApp')
       });
     };
 
+        $scope.todos = [
+      {
+        face : '/img/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : '/img/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : '/img/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : '/img/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : '/img/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+    ];
+
+      $scope.data = {
+    group1 : '2',
+    group2 : '6'
+  };
+  
+  $scope.data.cb1 = true;
+  $scope.data.cb2 = false;
 
     $scope.selectedIndex = 0;
   })
@@ -46,16 +91,16 @@ angular.module('genericChromeMdApp')
     };
   })
   .directive('ig', function() {
-    return {
-      restrict: 'E',
-      replace: true,
-      scope: {
-        fid: '@'
-      },
-      template: '<div class="material-input-group">' +
-                  '<label for="{{fid}}">Description</label>' +
-                  '<input id="{{fid}}" type="text" ng-model="data.description">' +
-                '</div>',
-    }
-  })
-  ;
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      fid: '@'
+    },
+    template: 
+      '<material-input-group>' +
+        '<label for="{{fid}}">Description</label>' +
+        '<material-input id="{{fid}}" type="text" ng-model="data.description">' +
+      '</material-input-group>'
+  };
+});
